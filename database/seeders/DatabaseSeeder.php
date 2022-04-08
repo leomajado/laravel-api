8aa4b82897c9c9f5f79d728d925a244cea404e07
LeoM <leomaj@hotmail.com>
2022-04-08 02:20:29 +0200

    Fix User Faker Add Test Case Post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Str;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\User::create([
+            'name' => 'Example',
+            'email' => 'user@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10)
+        ]);
+
         \App\Models\User::factory(10)->create();
         \App\Models\Posts::factory(50)->create();
     }
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,12 +2,13 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class ExampleTest extends TestCase
 {
-    /**
+     /**
      * A basic test example.
      *
      * @return void
@@ -18,4 +19,70 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    protected function authenticate(){
+
+        $usr = User::where('email','user@example.com')->first() ;
+
+        if(!empty($usr))
+            $usr->delete();
+
+        $user = User::create([
+            'name' => 'Example',
+            'email' => 'user@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10)
+        ]);
+
+        if (!auth()->attempt(['email'=>$user->email, 'password'=>'password'])) {
+            return response(['message' => 'Login credentials are invaild']);
+        }
+
+        return auth()->user()->createToken('Personal Access Token')->accessToken;
+
+    }
+
+    public function test_find_product()
+    {
+        $token = $this->authenticate();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $token,
+        ])->json('GET','api/post/1');
+
+        //Write the response in laravel.log
+        \Log::info(1, [$response->getContent()]);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_err_find_product()
+    {
+        $token = $this->authenticate();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $token,
+        ])->json('GET','api/post/9999');
+
+        //Write the response in laravel.log
+        \Log::info(1, [$response->getContent()]);
+
+        $response->assertStatus(404);
+    }
+
+    public function test_get_all_product()
+    {
+        $token = $this->authenticate();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $token,
+        ])->json('GET','api/posts');
+
+        //Write the response in laravel.log
+        \Log::info(1, [$response->getContent()]);
+
+        $response->assertStatus(200);
+    }
+
 }
